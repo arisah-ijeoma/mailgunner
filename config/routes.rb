1a2b195a68c5297451473d3680e2811f3c401ca4
Jay <gift_ij2002@yahoo.com>
2016-10-18 13:36:09 +0100

    use consistent styles in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'mailings#index'
   get :send_mail, to: 'mailings#send_mail', as: :send_mail
   get :view_mails, to: 'mailings#view_mails', as: :view_mails
-  match 'logs', to: 'stats#logs', via: :get
+  get :logs, to: 'stats#logs', as: :logs
 end
